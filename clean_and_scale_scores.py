7684b1f9eca166b43a51bc31186ff2e74b90a423
qsakaso <roman.prusinskii@gmail.com>
2025-12-03 16:15:10 +0100

    Update clean_and_scale_scores.py

--- clean_and_scale_scores.py
+++ clean_and_scale_scores.py
@@ -1,6 +1,21 @@
 import numpy as np
 
+
 def clean_and_scale_scores(scores, min_score, max_score):
-    clipped = np.clip(scores, min_score, max_score)
-    scaled = (clipped - min_score) / (max_score - min_score)
+    scoresMK1 = np.array(scores, dtype=float)
+    if scoresMK1.ndim == 1:
+        for i in range(len(scoresMK1)):
+            if scoresMK1[i] < min_score:
+                scoresMK1[i] = min_score
+            if scoresMK1[i] > max_score:
+                scoresMK1[i] = max_score
+    if scoresMK1.ndim == 2:
+        for i in range(scoresMK1.shape[0]):
+            for j in range(scoresMK1.shape[1]):
+                if scoresMK1[i,j] < min_score:
+                    scoresMK1[i,j] = min_score
+                if scoresMK1[i,j] > max_score:
+                    scoresMK1[i,j] = max_score
+
+    scaled = (scoresMK1 - min_score) / (max_score - min_score)
     return scaled.astype(float)
